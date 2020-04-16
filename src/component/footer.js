7934f6c726418855bfc2ee53cfb495cd2a11f465
mohame455 <61824199+mohame455@users.noreply.github.com>
2020-04-16 18:04:58 +0100

    modified

--- src/component/footer.js
+++ src/component/footer.js
@@ -2,25 +2,36 @@ import React, { Component } from 'react';
 class Footer extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            tab3: [{ href: "https://www.znanylekarz.pl/?_ga=2.256523918.447552144.1583917801-370134012.1583917801", town: "Poland," },
+            { href: "https://www.doktortakvimi.com/?_ga=2.81991955.447552144.1583917801-370134012.1583917801", town: " Turkey," },
+            { href: "https://www.doctoralia.es/", town: " Spain," },
+            { href: "https://www.miodottore.it/?_ga=2.149221811.447552144.1583917801-370134012.1583917801", town: "Italy," },
+            { href: "https://www.znamylekar.cz/", town: " Czech Republic," },
+            { href: "https://www.doctoralia.com.mx/", town: " Mexico," },
+            { href: "https://www.doctoralia.co/", town: " Colombia," },
+            { href: "http://www.doctoralia.com.br/", town: " Brazil," },
+            { href: "http://www.doctoraliar.com/", town: " Argentina" },
+            { href: "http://www.doctoralia.cl/", town: " Chile" }]
+        }
     }
-    render() { 
-        return ( <div className="section-final">
-            <p className="text-fin">We are leaders in 10 countries: <a href="https://www.znanylekarz.pl/?_ga=2.256523918.447552144.1583917801-370134012.1583917801">Poland,</a>
-                <a href="https://www.doktortakvimi.com/?_ga=2.81991955.447552144.1583917801-370134012.1583917801"> Turkey,</a>
-                <a href="https://www.doctoralia.es/"> Spain,</a>
-                <a href="https://www.miodottore.it/?_ga=2.149221811.447552144.1583917801-370134012.1583917801"> Italy,</a>
-                <a href="https://www.znamylekar.cz/"> Czech Republic,</a>
-                <a href="https://www.doctoralia.com.mx/"> Mexico,</a>
-                <a href="https://www.doctoralia.co/"> Colombia,</a>
-                <a href="http://www.doctoralia.com.br/"> Brazil,</a>
-                <a href="http://www.doctoraliar.com/"> Argentina</a> and
-                <a href="http://www.doctoralia.cl/"> Chile</a>
-            </p>
+    render() {
+        return (<div className="section-final">
+            <div className="sub-final">
+                <span className="text-fin">We are leaders in 10 countries:
+            {this.state.tab3.map(e => (e.href) ?
+
+                    <span><a href={e.href}>{e.town}</a></span> :
+
+                    <span>{e.town}</span>
+
+                )}
+                </span>
+            </div>
             <p className="text-fin">This site uses cookies to deliver services in accordance with this Privacy Policy. You can specify the conditions for storing or accessing cookies on your browser.</p>
             <p className="text-fin">www.docplanner.com © 2020</p>
-        </div> );
+        </div>);
     }
 }
- 
+
 export default Footer;
